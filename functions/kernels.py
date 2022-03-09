fd8cfc99d9c2d5ac321ca783d88d31fa89c620c8
Jaakko Koli <jaakko.koli@gmail.com>
2022-03-09 12:20:11 +0200

    more kernels

--- functions/kernels.py
+++ functions/kernels.py
@@ -1,17 +1,6 @@
 import numpy as np
 
-gridSize_1 = 8 #base 1
-gridSize_0 = 7 #base 0
-uniqueOptions = gridSize_1**2
-
 def rbf(x1,x2,theta):
-    #if np.shape(x1)[1] != np.shape(x2)[1]:
-    #    print("incorrect x1 and x2 input dimensions")
-    #    return
-    #elif any([x<0 for x in theta]):
-    #    print("all parameters must be greater than 0")
-    #    return
-    
     N1 = np.shape(x1)[0]
     N2 = np.shape(x2)[0]
     d = np.shape(x1)[1]
@@ -24,3 +13,43 @@ def rbf(x1,x2,theta):
     else:
         return sf*np.exp(-0.5 * sigma)
 		
+
+def shepard(x1, x2, theta):
+    N1 = np.shape(x1)[0]
+    N2 = np.shape(x2)[0]
+    d = np.shape(x1)[1]
+    sigma = np.zeros((N1, N2))
+    sf = theta[d]
+	p_minowski = theta[d+2]
+    for i in range(d):
+        sigma = sigma + ((abs(np.matrix((np.subtract.outer(x1[:,i],x2[:,i]))))**p_minowski)**(1/p_minowski)**p_minowski)/theta[i].T
+    if np.array_equal(x1,x2):
+        return sf*np.exp(-0.5 * sigma) + theta[d+1]*np.identity(N1)
+    else:
+        return sf*np.exp(-0.5 * sigma)
+		
+def oru(x1,x2,theta):
+    N1 = np.shape(x1)[0]
+    N2 = np.shape(x2)[0]
+    d = np.shape(x1)[1]
+    sigma = np.zeros((N1, N2))
+    sf = theta[d]
+    for i in range(d):
+        sigma = sigma + abs(np.matrix(np.subtract.outer(x1[:,i],x2[:,i])/theta[i])).T
+    if np.array_equal(x1,x2):
+        return sf*np.exp(-0.5 * sigma) + theta[d+1]*np.identity(N1)
+    else:
+        return sf*np.exp(-0.5 * sigma)
+		
+def matern(x1,x2,theta):
+    N1 = np.shape(x1)[0]
+    N2 = np.shape(x2)[0]
+    d = np.shape(x1)[1]
+    sigma = np.zeros((N1, N2))
+    sf = theta[d]
+    for i in range(d):
+        sigma = sigma + abs(np.matrix(np.subtract.outer(x1[:,i],x2[:,i])/theta[i])).T
+    if np.array_equal(x1,x2):
+        return sf*(1+(np.sqrt(3)*sigma))*exp(-np.sqrt(3)*sigma) + theta[d+1]*np.identity(N1)
+    else:
+        return sf*(1+(np.sqrt(3)*sigma))*exp(-np.sqrt(3)*sigma)
